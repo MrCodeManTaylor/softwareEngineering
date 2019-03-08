50869134ca7eb1c4f3d09270fc8ae315380d5214
Mitchell Taylor <mitchellthomastaylor@gmail.com>
2019-03-07 23:19:49 -0500

    Major update to session control mechanism

--- StocksNStuff/src/stocksnstuff/UserGuiMain/UserGUI.java
+++ StocksNStuff/src/stocksnstuff/UserGuiMain/UserGUI.java
@@ -5,6 +5,8 @@
  */
 package stocksnstuff.UserGuiMain;
 
+import stocksnstuff.GuestGuiMain.GuestGUI;
+
 /**
  *
  * @author mtaylo35
@@ -33,10 +35,18 @@ public class UserGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         username = new javax.swing.JLabel();
+        logout = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         username.setText("User");
+
+        logout.setText("Logout");
+        logout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                logoutActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -46,11 +56,15 @@ public class UserGUI extends javax.swing.JFrame {
                 .addGap(137, 137, 137)
                 .addComponent(username)
                 .addContainerGap(241, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(logout))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(40, 40, 40)
+                .addComponent(logout)
+                .addGap(17, 17, 17)
                 .addComponent(username)
                 .addContainerGap(246, Short.MAX_VALUE))
         );
@@ -58,8 +72,19 @@ public class UserGUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void logoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logoutActionPerformed
+        // TODO add your handling code here:
+        
+        //We need to terminate the session...
+        
+        this.dispose();
+        GuestGUI uG = new GuestGUI();
+        uG.setVisible(true);
+    }//GEN-LAST:event_logoutActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton logout;
     private javax.swing.JLabel username;
     // End of variables declaration//GEN-END:variables
 }
